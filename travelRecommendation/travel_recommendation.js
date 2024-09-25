8621ce8476c4c76b9f42df66014a3cb60828e243
Miguel Angel López Guzmán <lg10migue@gmail.com>
2024-09-24 21:56:32 -0600

    Create Atlas Advisor webpage.
    Added the code for the webpage.

---
+++ travelRecommendation/travel_recommendation.js
@@ -0,0 +1,99 @@
+// Get the button elements.
+const searchButton = document.getElementById("search-btn");
+const clearButton = document.getElementById("clear-btn");
+const searchInput = document.getElementById("search-input");
+const homeContent = document.getElementById("content-container");
+const recommendationsContainer = document.getElementById("recommendations");
+
+function fetchTravelData() {
+    fetch("travel_recommendation.json")
+        .then((response) => response.json())
+        .then((data) => searchRecommendations(data))
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+function searchRecommendations(fullData) {
+    const searchValue = searchInput.value.toLowerCase();
+    let filteredData = [];
+    if (searchValue === "") {
+        alert("Please enter a search value.");
+        return;
+    } else if (searchValue.includes("countries")) {
+        alert("Please enter a valid search value.");
+        return;
+    }
+
+    // Search by category.
+    Object.keys(fullData).forEach((key) => {
+        if (key.toLowerCase().includes(searchValue)) {
+            filteredData = fullData[key];
+        }
+    });
+
+    // Search by country.
+    fullData.countries.forEach((country) => {
+        if (country.name.toLowerCase().includes(searchValue)) {
+            filteredData = country.cities;
+        }
+    });
+
+    if (filteredData.length === 0) {
+        alert("No recommendations found.");
+        return;
+    }
+
+    displayRecommendations(filteredData);
+}
+
+function displayRecommendations(dataArray) {
+    // Clear current recommendations.
+    clearRecommendations();
+
+    // Add a class to the content container to display the recommendations in two columns.
+    homeContent.classList.add("two-columns");
+
+    // Display the filtered data.
+    dataArray.forEach((recommendation) => {
+        const recommendationElement = document.createElement("div");
+        recommendationElement.classList.add("recommendation");
+        recommendationElement.innerHTML = `
+            <img src="images/${recommendation.imageUrl}" alt="${
+            recommendation.name
+        }" />
+            <h2>${recommendation.name}</h2>
+            <p>${recommendation.description}</p>
+            <p><b>Current Time: ${getCurrentTime(
+                recommendation.timeZone
+            )}</b></p>
+            <button type="button" class="book-btn">Visit!</button>
+        `;
+        recommendationsContainer.appendChild(recommendationElement);
+    });
+}
+
+function clearRecommendations() {
+    // Clear the recommendations.
+    homeContent.classList.remove("two-columns");
+    recommendationsContainer.innerHTML = "";
+}
+
+function clearSearchBar() {
+    searchInput.value = "";
+}
+
+function getCurrentTime(timeZone) {
+    const date = new Date();
+    const options = {
+        timeZone: timeZone,
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true,
+    };
+    return date.toLocaleDateString("en-US", options);
+}
+
+searchButton.addEventListener("click", fetchTravelData);
+clearButton.addEventListener("click", clearSearchBar);
